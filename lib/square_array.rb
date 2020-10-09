9a6464d8c8f625e1ac88159d070e231a879e7540
Aaron Amodt <aaron@amodttech.com>
2020-10-09 18:13:57 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,14 +1,15 @@
 def square_array(array)
   count = array.length 
-  
+  count.times do |index|
+    array
 end
 
 
 
 
-array = [1,2,3,4,5]
-length = array.length
+#array = [1,2,3,4,5]
+#length = array.length
  
-length.times do |index|
-  puts array[index]
-end
+#length.times do |index|
+#  puts array[index]
+#end
